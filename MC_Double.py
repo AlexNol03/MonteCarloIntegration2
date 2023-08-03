a98c0f083afe33a5bf7db2789ba58163d7fecadb
AlexNol03 <136399183+AlexNol03@users.noreply.github.com>
2023-08-02 18:14:51 -0700

    Update MC_Double.py

--- MC_Double.py
+++ MC_Double.py
@@ -7,74 +7,54 @@ Created on Wed Jul 12 18:32:29 2023
 
 import numpy as np
 
-def MonteCarlo_double(f, g, x0, x1, y0, y1, n):
-    
-    '''
-    Monte Carlo integration of f over a domain g>=0, embedded
-    in a rectangle [x0,x1]x[y0,y1]. n^2 is the number of
-    random points.
-    '''
-    
-    # Draw n**2 random points in the rectangle
-    
-    x = np.random.uniform(x0, x1, n)
-    y = np.random.uniform(y0, y1, n)
-    
-    # Compute sum of f values inside the integration domain
-    
-    f_mean = 0
-    num_inside = 0   # number of x,y points inside domain (g>=0)
-    
+def MCinteg(f, x0, x1, y0, y1, n):
+    """
+   
+
+    Parameters
+    ----------
+    f : TYPE float function
+        DESCRIPTION.function to be integrated
+    x0 : TYPEfloat
+        DESCRIPTION. on the x-axis where to start integral
+    x1 : TYPE float
+        DESCRIPTION. on the x - axis where to stop integral
+    y0 : TYPE float
+        DESCRIPTION. bottom of rectangle, y=y0
+    y1 : TYPE float
+        DESCRIPTION. top of the rectangle
+    n : TYPE integrer
+        DESCRIPTION. n**2 points in the rectangle
+
+    Returns
+    -------
+    integral of f from x0 to x1
+   
+    """
+   
+    x = np.random.random(n)*(x1-x0)+x0
+    y = np.random.random(n)*(y1-y0)+y0
+   
+    areaCount = 0
+   
     for i in range(len(x)):
+        xVal = x[i]
         for j in range(len(y)):
-            if g(x[i], y[j]) >= 0:
-                num_inside = num_inside + 1
-                f_mean = f_mean + f(x[i], y[j])
-    f_mean = f_mean/num_inside
-    area = num_inside/(n**2)*(x1 - x0)*(y1 - y0)
-    
-    return area*f_mean
+                yVal = y[j]
+                if f(xVal) < yVal: areaCount +=1
+   
+    area = areaCount/(n**2)*(x1-x0)*(y1-y0)
+    return area
 
-def test_MonteCarlo_double_rectangle_area():
-    '''Check the area of a rectangle.'''
-    
-    def g(x, y):
-        return (1 if (0 <= x <= 2 and 3 <= y <= 4.5) else -1)
-
-    x0 = 0;  x1 = 3;  y0 = 2;  y1 = 5  # embedded rectangle
-    n = 1000
-    np.random.seed(8)      # must fix the seed!
-    I_expected = 3.121092  # computed with this seed
-    I_computed = MonteCarlo_double(
-        lambda x, y: 1, g, x0, x1, y0, y1, n)
-    assert abs(I_expected - I_computed) < 1E-14
-
-def test_MonteCarlo_double_circle_r():
-    '''Check the integral of r over a circle with radius 2.'''
-    
-    def g(x, y):
-        xc, yc = 0, 0  # center
-        R = 2          # radius
-        return  R**2 - ((x-xc)**2 + (y-yc)**2)
-
-    # Exact: integral of r*r*dr over circle with radius R becomes
-    # 2*pi*1/3*R**3
-    import sympy
-    r = sympy.symbols('r')
-    I_exact = sympy.integrate(2*sympy.pi*r*r, (r, 0, 2))
-    print('Exact integral: {:g}'.format(I_exact.evalf()))
-    x0 = -2;  x1 = 2;  y0 = -2;  y1 = 2
-    n = 1000
-    np.random.seed(6)
-    I_expected = 16.7970837117376384  # Computed with this seed
-    I_computed = MonteCarlo_double(
-        lambda x, y: np.sqrt(x**2 + y**2),
-        g, x0, x1, y0, y1, n)
-    
-    print('MC approximation, {:d} samples: {:.16f}'\
-          .format(n**2, I_computed))
-    assert abs(I_expected - I_computed) < 1E-15
-
-if __name__ == '__main__':
-    test_MonteCarlo_double_rectangle_area()
-    test_MonteCarlo_double_circle_r()
+if __name__ == "__main__":
+   
+    def f(x):
+        return x
+    x0=0
+    x1 =1
+    y0=0
+    y1=1
+    n=500
+   
+    area = MCinteg(f, x0, x1, y0, y1, n)
+    print(area)
